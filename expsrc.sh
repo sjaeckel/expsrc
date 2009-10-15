f03b8599121738ea55e055c868093739cb11cca2
Nathan Braun <braun@stzedn.de>
2009-10-15 14:35:39 +0200

    Command line options parsing added.
    Currently supporting -i|--repo and -o|--output for the output directory

--- expsrc.sh
+++ expsrc.sh
@@ -4,9 +4,17 @@
 
 _usage() 
 {
-  echo "This script exports the source of a repository with revision string in the code"
-  echo "usage: ${0##*/} <to> <from>"
-  exit -1
+	echo -e
+	echo -e "${0##*/} version `git describe --tags --always`"
+	echo -e "Copyright Steinbeis Transfer Center Embedded Design and Networking, 2009"
+	echo -e
+	echo "This script exports the source of a repository with revision string in the code"
+	echo -e
+	echo -e "Usage: ${0##*/}[options] <destination> <source>"
+	echo -e "\t-i|--repo\tBase directory of the repository to export"
+	echo -e "\t-o|--output\tDirectory where to output the files"
+  echo -e "\t-h|--help\tThis help"
+	exit -1
 }
 
 _colored_echo()
@@ -52,6 +60,56 @@ _colored_echo()
   echo
 }
 
+_check_params()
+{
+	option=$1	
+	
+	case $option in
+		"-h")
+						_usage
+						exit 0;;
+		"--help")
+						_usage
+						exit 0;;
+		"-p")
+						echo "-p option"
+						check_params_ret=2;;
+            
+    "-i")
+            inFolder="$2"
+            check_params_ret=2;;
+    "-repo")
+            inFolder="$2"
+            check_params_ret=2;;
+    "-o")
+            outFolder="$2"
+            check_params_ret=2;;
+
+    "-output")
+            outFolder="$2"
+            check_params_ret=2;;
+		*)				
+						_colored_echo red Unknown option $1
+						_usage
+						exit -1;;
+	esac
+
+}
+
+#-------------------------------------------------------------------------------
+# This function checks, whether the given file should be parsed or not
+#
+# @param   1 name of the file to check against the patter
+# @return  check_parse_ret set to 1, when the file is to be parsed, otherwise
+#          set to 0
+#-------------------------------------------------------------------------------
+_check_parse()
+{
+  
+  
+  check_parse_ret=0
+}
+
 ###############################################################################
 #                               GLOBAL VARIABLES                              #
 ###############################################################################
@@ -62,24 +120,89 @@ BASEFOLDER=$PWD
 # rememeber my full name to call me later
 THIS=${PWD}/${0##*/}
 
+# input directory
+# This is the directory of the project which is to be exported.
+# If not given, the directory
+inFolder=
+
+# output directory
+outFolder=
+
+
+# array of patterns for files to include in parsing. If left empty
+# all files are parsed
+arr_parseFiles=
+
+
+
 ###############################################################################
 #                                  MAIN BODY                                  #
 ###############################################################################
 
+
+# Parse command line arguments
+i=1
+passes=$#
+
+if [ $passes -gt 0 ]
+then
+	while [ $i -le $passes ]
+	 do
+		option="$1"
+		param="$2"
+
+    # Check if the option starts with a leading '-' then it is an 
+		# option, otherwise we reached the end of the options and can
+		# continue with the script
+		
+		if [[ $option == -* ]]
+		then	
+			_check_params "$option" "$param"
+					
+			i=$((check_params_ret+1))	
+			shift $check_params_ret		
+		else
+			echo "end of options reached"
+			break;
+		fi
+
+	done
+fi
+
 # choose folder from where to fetch input data
-case "$#" in
-  2)
-    inFolder="$2"
-    ;;
-  1|0)
-    inFolder=${PWD%/${PWD##*/}}
-    echo ""
-    _colored_echo green "*** Exporting repository ${inFolder##*/}"
-    ;;
-  *)
-    _usage
-    ;;
-esac
+if [ -z "$inFolder" ]
+then
+  case "$#" in
+    2)
+      inFolder="$2"
+      ;;
+    1|0)
+      inFolder=${PWD%/${PWD##*/}}
+      ;;
+    *)
+      _usage
+      ;;
+  esac
+fi
+
+# choose folder where to put output data
+if [ -z "$outFolder" ]
+then
+  case "$#" in
+    2|1)
+      outFolder="$1"
+      ;;
+    0)
+      outFolder="$BASEFOLDER/expsrc_${git_version}_`date -u +%y%m%d_%H%M%S`"
+      ;;
+    *)
+      _usage
+      ;;
+  esac
+fi
+
+_colored_echo green "*** Exporting repository : ${inFolder##*/}"
+_colored_echo green "*** Target folder        : ${outFolder##*/}"
 
 cd $inFolder
 
@@ -90,19 +213,6 @@ fi
 
 # get version string
 git_version=`git describe --tags --always`
-
-# choose folder where to put output data
-case "$#" in
-  2|1)
-    outFolder="$1"
-    ;;
-  0)
-    outFolder="$BASEFOLDER/expsrc_${git_version}_`date -u +%y%m%d_%H%M%S`"
-    ;;
-  *)
-    _usage
-    ;;
-esac
 
 # create this folder if necessary, suppress error
 mkdir -p $outFolder 2>/dev/null
